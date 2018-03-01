c8682d1c795c50c3118686d7aeebd2c87c9ddb51
Alex Billington <alex@McBook2.lan>
2018-03-01 20:39:25 +0000

    Initial commit

---
+++ _compass/config.rb
@@ -0,0 +1,24 @@
+#require 'zurb-foundation'
+# Require any additional compass plugins here.
+
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../assets/css"
+sass_dir = "sass"
+images_dir = "../assets/img"
+javascripts_dir = "../assets/js"
+output_style = :expanded
+#output_style = :compressed
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+generated_images_dir = "../assets/img/spr";
+sprite_load_path = "sprites/"
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
